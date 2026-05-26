3a3f167b96e00f233a2a190df43c0d26bf69b5a7
Bastien Antraygues <bastien-toulouse@outlook.fr>
2026-05-26 15:45:58 +0200

    Long Text static can be change on the future into a api

---
+++ src/texts/AboutText.tsx
@@ -0,0 +1,8 @@
+export const aboutText = `
+Je suis un développeur fullstack passionné par la création d'applications web innovantes et 
+performantes. Avec une solide expérience en développement frontend et backend, je maîtrise les 
+technologies modernes telles que React, Node.js, et MongoDB. Mon objectif est de concevoir des 
+solutions efficaces qui répondent aux besoins des utilisateurs tout en offrant une expérience 
+fluide et agréable. Je suis constamment à la recherche de nouveaux défis pour améliorer mes compétences 
+et contribuer à des projets passionnants.
+`
