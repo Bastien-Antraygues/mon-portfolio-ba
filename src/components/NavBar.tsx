eec89710d72889ae20491829e1c0eb396f003132
Bastien Antraygues <bastien-toulouse@outlook.fr>
2026-06-05 15:54:03 +0200

    Changement des couleurs utilisation des darkGray

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 export function NavBar(){
     return(
-        <nav className="fixed bg-vs-dark-1 p-4 m-4 rounded-3xl md:w-[50%] lg:w-[40%] mx-[30%]">
+        <nav className="fixed bg-darkGray-5 p-4 m-4 rounded-3xl md:w-[50%] lg:w-[40%] mx-[30%]">
             <div className="flex gap-4 justify-center">
                 <a href="#home">Accueil</a>
                 <a href="#propos">À propos</a>
