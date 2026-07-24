f0e04a7e2891a4f58a7b0ddf64b91aa9358e8036
Bastien Antraygues <bastien-toulouse@outlook.fr>
2026-07-24 14:45:20 +0200

    Ajout de la section ProjectComponent

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import { AboutComponent } from "../components/AboutComponent";
 import { CompetenceComponent } from "../components/CompetenceComponent";
 import { HomeComponent } from "../components/HomeComponent";
+import { ProjectComponent } from "../components/ProjectComponent";
 
 export function HomePage(){
     return(
         <>
             <HomeComponent/>
             <AboutComponent/>
+            <ProjectComponent/>
             <CompetenceComponent/>
         </>
     )
