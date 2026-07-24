5da1f5ff99ddc3ac582771c878d21c90e18d90fb
Bastien Antraygues <bastien-toulouse@outlook.fr>
2026-07-24 14:42:49 +0200

    NavBar necesite d'être corriger. Changement url ajout du /

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,12 @@ export function NavBar(){
     return(
         <nav className="fixed bg-darkGray-5 p-4 m-4 rounded-3xl md:w-[50%] lg:w-[40%] mx-[30%]">
             <div className="flex gap-4 justify-center">
-                <a href="#home">Accueil</a>
-                <a href="#propos">À propos</a>
-                <a href="#competences">Compétences</a>
-                <a href="#projet">Projet</a>
-                <a href="#contact">Contact</a>
+                <a href="/#home">Accueil</a>
+                <a href="/#propos">À propos</a>
+                <a href="/#projet">Projet</a>
+                <a href="/#competences">Compétences</a>
+                
+                <a href="/#contact">Contact</a>
             </div>    
         </nav>
     )
