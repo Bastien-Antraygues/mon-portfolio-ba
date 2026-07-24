f06d22f6581229fe51a6d0f5cc789c019afdd3ce
Bastien Antraygues <bastien-toulouse@outlook.fr>
2026-07-24 14:44:57 +0200

    Ajout de 3 autre pages correspondant au contenu des projets

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import './App.css'
 import { NavBar } from './components/NavBar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
+import { CvPdfPage } from './pages/CvPdfPage'
+import { PokemonAPIPage } from './pages/PokemonAPIPage'
+import { HackathonYnovPage } from './pages/HackathonYnovPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +17,9 @@ function App() {
       <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/projects/cvpdf" element={<CvPdfPage/>}/>
+        <Route path="/projects/pokemonapi" element={<PokemonAPIPage/>}/>
+        <Route path="/projects/hackathonynov" element={<HackathonYnovPage/>}/>
       </Routes>
     </BrowserRouter>
   )
